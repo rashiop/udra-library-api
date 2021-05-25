ebcbd7b0aa6894a26adc24dabf7efd488cd4c8f4
rashiop <red.alpacca@gmail.com>
2021-05-25 09:40:37 +0700

    chore: yarn update depedency & try chaos engineering

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { json, urlencoded } from 'body-parser';
+import ChaosMonkey from 'chaos-monkey';
 import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
@@ -16,23 +17,26 @@ import { userRouter } from './resources/user';
 
 export const app = express()
 
+new ChaosMonkey(app, require('./lib/chaosMonkey') ).start();
+
 app.use(cors())
 app.use(helmet())
 app.use(json())
 app.use(urlencoded({ extended: true}))
 app.use(morganMiddleware)
 
+
 app.use('/signin', signin)
 app.use('/signup', signup)
 
-app.use('/api', protect)
+// app.use('/api/', protect)
 app.use('/api/v1/author', authorRouter)
 app.use('/api/v1/book', bookRouter)
-app.use('/api/v1/fine', fineRouter)
+app.use('/api/v1/fine', protect, fineRouter)
 app.use('/api/v1/genre', genreRouter)
 app.use('/api/v1/publisher', publisherRouter)
-app.use('/api/v1/user', userRouter)
-app.use('/api/v1/book-transaction', bookTransactionRouter)
+app.use('/api/v1/user', protect, userRouter)
+app.use('/api/v1/book-transaction', protect, bookTransactionRouter)
 
 
 app.use((err, _req, res, next) => {
@@ -40,4 +44,5 @@ app.use((err, _req, res, next) => {
   errorHandler(err, res)
 })
 
-app.use('*', (_req, res) => res.status(404).end())
+
+// app.use('*', (_req, res) => res.status(404).end())
