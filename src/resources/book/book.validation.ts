385e8c3a1e7d449be8d32cd7ab5d25b966ec2697
rashiop <red.alpacca@gmail.com>
2021-06-07 22:58:47 +0700

    chore(deprecated warning): fix express-validator/check warning

--- src/resources/book/book.validation.ts
+++ src/resources/book/book.validation.ts
@@ -1,11 +1,11 @@
-import { body } from 'express-validator/check';
+import { body } from 'express-validator';
 
 import { error } from './book.constant';
 
 const saveBook = [
   body('title', error.titleRequired).exists(),
   body('authors', error.authorRequired).exists(),
-  body('genre', error.genreRequired).exists(),
+  body('genres', error.genreRequired).exists(),
   body('publisher', error.publisherRequired).exists(),
 ]
 
--- src/resources/bookTransaction/bookTransaction.validation.ts
+++ src/resources/bookTransaction/bookTransaction.validation.ts
@@ -1,4 +1,4 @@
-import { body } from 'express-validator/check';
+import { body } from 'express-validator';
 
 import { error } from './bookTransaction.constant';
 
