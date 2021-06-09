6227619b7cd9d06e3ae0d1696998d62c837d697b
rashiop <red.alpacca@gmail.com>
2021-06-09 23:08:27 +0700

    fix: default lang error

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,19 @@
 import customEnv from 'custom-env';
 
-const inDeployment = Boolean(process.env.NODE_ENV)
-if (customEnv && !inDeployment) {
+if (customEnv) {
   customEnv.env(true)
 }
 
 export default {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 8080,
-    language: process.env.DEFAULT_LANGUAGE || 'en',
-    dbUri: process.env.DB_URL || '',
+    language: process.env.DEFAULT_LANGUAGE || 'EN',
+    dbUri: `${process.env.DB_URL || ''}`,
+    dbSetting: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    },
     secrets: {
         jwt: process.env.JWT_SECRET || 'secret',
         jwt_exp: '100d',
