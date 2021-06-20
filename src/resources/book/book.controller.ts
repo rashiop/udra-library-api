b705ae4cd2265837cadfd7208c68ff8f72df2756
rashiop <red.alpacca@gmail.com>
2021-06-20 20:08:19 +0700

    ref(book): removed unused controller

--- src/resources/book/book.controller.ts
+++ src/resources/book/book.controller.ts
@@ -1,29 +1,4 @@
-import { Request, Response } from 'express';
-
 import { crudController } from '../../helper';
 import Book from './book.model';
-
-export const getOneById = async(req: Request, res: Response) => {
-  try {
-    const bookId = req.params.id;
-    const book = await Book.getOneById(bookId)
-    return res.status(200).json({ data: book })
-  } catch({ httpCode = 400, message }) {
-    return res
-      .status(httpCode)
-      .json({ message, error: true })
-  }
-}
-
-export const getMany = async(_: Request, res: Response) => {
-  try {
-    const books = await Book.getMany();
-    return res.status(200).json({ data: books })
-  } catch({ httpCode = 400, message }) {
-    return res
-      .status(httpCode)
-      .json({ message, error: true })
-  }
-}
 
 export default crudController(Book)
--- src/resources/book/book.route.ts
+++ src/resources/book/book.route.ts
@@ -3,14 +3,14 @@ import { Router } from 'express';
 import { validateController } from '../../helper';
 import { checkPermissionRole, protect } from '../../lib/auth';
 import { Role } from '../user';
-import bookController, { getMany, getOneById } from './book.controller';
+import bookController from './book.controller';
 import validate from './book.validation';
 
 const router = Router();
 
 router
   .route('/')
-  .get(getMany)
+  .get(bookController.getMany)
   .post(
     validate.saveBook,
     validateController(),
@@ -21,7 +21,7 @@ router
 
 router
   .route('/:id')
-  .get(getOneById)
+  .get(bookController.getOneById)
   .put(
     validate.saveBook,
     validateController(),
